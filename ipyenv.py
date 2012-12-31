46f3269445c200507166295bce6d969cf5971e52
ymat <drowse314@gmail.com>
2013-01-01 00:03:54 +0900

    Add LICENSE lines and file.

--- ipyenv.py
+++ ipyenv.py
@@ -1,4 +1,13 @@
 # encoding: utf-8
+"""
+    ipyenv
+    ~~~~~~
+
+    A simple and poor environment supplyer for Python development.
+
+    :copyright: (c) 2014 by drowse314-dev-ymat@github.com.
+    :licence: BSD, see LICENCE for more details.
+"""
 
 import sys
 import os
